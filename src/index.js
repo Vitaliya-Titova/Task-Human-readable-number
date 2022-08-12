e2026219a07f3dc9d0e2730e215e82d35220469b
Vitaliya Titova <83167509+Vitaliya-Titova@users.noreply.github.com>
2022-08-12 18:32:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ module.exports = function toReadable (number) {
     if (number < 20) {
         return arr[number];
     } 
-    let num = Math.floor(number / 10);
+    let res = (Math.floor(number / 10))-2;
     if (number < 100 && number % 10 === 0) {
-        return arr2[num-2];
+        return arr2[res];
     }
     if (number < 100) {
-        return (arr2[num-2] + ' ' + arr[number % 10]);}
+        return (arr2[res] + ' ' + arr[number % 10]);}
    let hun = Math.floor(number / 100);
    let n100 = Math.floor(number % 100 / 10);
    let n10 = number % 100;
